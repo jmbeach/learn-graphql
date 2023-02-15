cd327095fe3e390c41f1e7570c34d52b9d9186c3
jared.beach <jared.beach@capitalone.com>
2023-02-15 09:13:02 -0500

    get job by id

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -2,6 +2,7 @@ import { Job, Company } from "./db.js";
 export const resolvers = {
   Query: {
     jobs: () => Job.findAll(),
+    job: (_root, { id }) => Job.findById(id),
   },
   Job: {
     company: (job) => Company.findById(job.companyId),
