c3849ba503ee50b4614a0dbb612bd6534c7df44a
jared.beach <jared.beach@capitalone.com>
2023-02-14 22:10:41 -0500

    fetch jobs

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -1,12 +1,19 @@
-import JobList from './JobList';
-import { jobs } from '../fake-data';
+import JobList from "./JobList";
+import { getJobs } from "../graphql/queries";
+import { useEffect, useState } from "react";
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setJobs(await getJobs());
+    })();
+  }, [setJobs]);
+
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
